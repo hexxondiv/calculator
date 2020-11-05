1323c4736cf0eba13c1a70c832eaa68aa0bf501d
James <james@winexviv.com>
2020-11-05 18:26:44 +0100

    Took care of Issue #1

--- src/CalculatorController.php
+++ src/CalculatorController.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 class CalculatorController extends Controller
 {
     public function add($a,$b){
+        if(!is_numeric($a)||!is_numeric($b)) {
+            $result = "One of the parameters is not a number";
+            return  $result;
+        }
+        else
+
         $result= $a + $b;
         try{
             return view('calculator::add',compact('result'));
@@ -18,6 +24,10 @@ class CalculatorController extends Controller
     }
 
     public function subtract($a,$b){
-        echo $a - $b;
+        if(is_nan($a)||is_nan($b))
+            $result="One of the parameters is not a number";
+        else
+
+            echo $a - $b;
     }
 }
